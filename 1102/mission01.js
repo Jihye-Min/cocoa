6cf529a0a38f2229fb1cfe30bd0113fdddc790fe
Jihye-Min <67731613+Jihye-Min@users.noreply.github.com>
2020-11-03 16:10:25 +0900

    Update mission01.js

--- 1102/mission01.js
+++ 1102/mission01.js
@@ -1,50 +1,110 @@
 console.log("hello world!");
 
 
+var log_sequence = "";  //로그 기록용 전역변수
+
+
 //입력받기
-const readline = require('readline');
+const readline = require("readline"); 
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const rl = readline.createInterface({ 
+	input: process.stdin, output: process.stdout
+    }); 
 
-rl.on('line', function(line) {
-  rl.close();
-}).on("close", function() {
-  process.exit();
-});
+    //end 입력받을때까지 무한히 반복
+    //while(1)
+{
+    rl.on("line", function(line) {
+        var info = line.split(',');
+        
+        if(info[0] != "circle" && info[0] != "rect" && info[0] != "trap")
+        {
+            logStr = "잘못 입력";
+            console.log(logStr);
+            setLog(logStr + ":" + info[0]);
+        }
+        if(info[0] == "end")
+        {
+            process.exit();    
+        }
+        getArea(info[0], info[1], info[2], info[3]);
+        
+
+        printExecutionSequence();
+        rl.close(); 
+        })
+        .on("close", function() {
+        process.exit();
+    });
+}
 
 
 
-//도형 타입을 받는 함수
+//도형 타입을 분류하고 로그기록&결과 출력하는 함수
 function getArea(type, num1, num2, num3)
 {
-    result = type + " 넓이 : ";
+    result = type + " 넓이:";
+
     if(type == "circle")
     {
-        result += getCircle(a);
-
+        if(num2 == "undefined")
+        {
+            result += getCircle1(num1);
+        }
+        else
+        {
+            result += getCircle2(num1, num2);
+        }   
     }
     else if(type == "rect")
     {
-
+        result += getRect(num1, num2);
+        
     }
+    else if(type == "trap")
+    {
+        result += getTrapezoid(num1, num2, num3);
+    } 
     console.log(result);
+
+    setLog(result);
 }
 
-//도형별 넓이를 구하는 함수 (총 3종)
-function getCircle(r)
+
+//도형별 넓이를 구하는 함수 (총 4종)
+function getCircle1(r)
 {
     return r * r * Math.PI;
 }
 
-function getRect(x, y)
+function getCircle2(r, n)
 {
-    return x * y;
+    sum = 0;
+    for(i = r; i <= n; i ++)
+    {
+        sum += i * i * Math.PI;
+    }
+    return sum;
+}
+
+function getRect(a, b)
+{
+    return a * b;
 }
 
 function getTrapezoid(a, b, h)
 {
     return (a + b) * h / 2;
+}
+
+
+//로그 기록 관련 함수 (총 2종)
+function setLog(log)
+{
+    log_sequence += log + ",";
+}
+
+function printExecutionSequence()
+{
+    console.log(log_sequence);
 }
