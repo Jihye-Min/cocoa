13a82ac44f9b9f03fa6242a9fdbc78f757ee39fd
Jihye-Min <67731613+Jihye-Min@users.noreply.github.com>
2020-11-08 18:38:34 +0900

    데이터 export로 변경

--- mssion/week1-2/misson03.js
+++ mssion/week1-2/misson03.js
@@ -129,130 +129,25 @@ function printName(data)
 
 
 //Test Case - 1. factorial 함수
-console.log("##### Test Case - 1. factorial 함수 #####")
+console.log("##### Test Case - 1. factorial 함수 #####");
 console.log(calculate(4).join());
 
 //Test Case - 2. 배열 거르기
-console.log("\n\n##### Test Case - 2. 배열 거르기 #####")
+console.log("\n\n##### Test Case - 2. 배열 거르기 #####");
 const peoples = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"];
 filterId(peoples);
 
 //Test Case - 3. 평균 구하기
-console.log("\n\n##### Test Case - 3. 평균 구하기 #####")
+console.log("\n\n##### Test Case - 3. 평균 구하기 #####");
 const grades = [[88,76,77], [33,44,44], [90,100,94], [30,44,98]];
 printScore(grades);
 
 //Test Case - 4. 배열 만들기
-console.log("\n\n##### Test Case - 4. 배열 만들기 #####")
-const data = {
-    "debug": "on",
-    "window": {
-        "title": "Sample Konfabulator Widget",
-        "name": "main_window",
-        "width": 500,
-        "height": 500
-    },
-    "image": { 
-        "src": "Images/Sun.png",
-        "name": "sun1",
-        "hOffset": 250,
-        "vOffset": 250,
-        "alignment": "center"
-    },
-    "text": {
-        "data": "Click Here",
-        "size": 36,
-        "style": "bold",
-        "name": "text1",
-        "hOffset": 250,
-        "vOffset": 100,
-        "alignment": "center",
-        "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
-    }
-}
+console.log("\n\n##### Test Case - 4. 배열 만들기 #####");
+var data = require('./o.js');
 printKey(data);
 
 //Test Case - 5. 배열 결과 출력
-console.log("\n\n##### Test Case - 5. 배열 결과 출력 #####")
-const data2 = 
-[{
-	"id": 1,
-	"name": "Yong",
-	"phone": "010-0000-0000",
-	"type": "sk",
-	"childnode": [{
-		"id": 11,
-		"name": "echo",
-		"phone": "010-0000-1111",
-		"type": "kt",
-		"childnode": [{
-				"id": 115,
-				"name": "hary",
-				"phone": "211-1111-0000",
-				"type": "sk",
-				"childnode": [{
-					"id": 1159,
-					"name": "pobi",
-					"phone": "010-444-000",
-					"type": "kt",
-					"childnode": [{
-							"id": 11592,
-							"name": "cherry",
-							"phone": "111-222-0000",
-							"type": "lg",
-							"childnode": []
-						},
-						{
-							"id": 11595,
-							"name": "solvin",
-							"phone": "010-000-3333",
-							"type": "sk",
-							"childnode": []
-						}
-					]
-				}]
-			},
-			{
-				"id": 116,
-				"name": "kim",
-				"phone": "444-111-0200",
-				"type": "kt",
-				"childnode": [{
-					"id": 1168,
-					"name": "hani",
-					"phone": "010-222-0000",
-					"type": "sk",
-					"childnode": [{
-						"id": 11689,
-						"name": "ho",
-						"phone": "010-000-0000",
-						"type": "kt",
-						"childnode": [{
-								"id": 116890,
-								"name": "wonsuk",
-								"phone": "010-000-0000",
-								"type": "kt",
-								"childnode": []
-							},
-							{
-								"id": 1168901,
-								"name": "chulsu",
-								"phone": "010-0000-0000",
-								"type": "sk",
-								"childnode": []
-							}
-						]
-					}]
-				}]
-			},
-			{
-				"id": 117,
-				"name": "hong",
-				"phone": "010-0000-0000",
-				"type": "lg",
-				"childnode": []
-			}
-		]
-	}]
-}];
+console.log("\n\n##### Test Case - 5. 배열 결과 출력 #####");
+var data2 = require('./json_tree.js');
 printName(data2);
